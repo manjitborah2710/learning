d3477939ea6ecd45cf1b07c3ef34f61bd16dbe67
ManjiT <manjitborah2710@gmail.com>
2023-05-27 13:23:18 +0530

    wip

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,11 +2,16 @@ from kafka import KafkaProducer
 import json
 import sys
 import data
+import time
 
 def json_serializer(value):
-    return json.dumps(value)
+    return json.dumps(value).encode('utf-8')
 
-producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
+def send_to_kafka(producer, message, topic):
+    producer.send(topic, message)
+
+
+producer = KafkaProducer(bootstrap_servers = ['localhost:9092'], value_serializer = json_serializer)
 
 count = 5
 try:
@@ -17,4 +22,6 @@ except:
 while(count > 0):
     user = data.get_user()
     print(user)
+    send_to_kafka(producer, user, 'test_topic')
+    time.sleep(4)
     count = count - 1
